92580fa2a83eb9b08c65ce06d630f1d8d377c9fa
Nikolay1257 <otraskinnikolaj@gmail.com>
2023-02-26 11:36:13 +0100

    upload device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a12.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a12-user \
+    omni_a12-userdebug \
+    omni_a12-eng
---
+++ omni_a12.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from a12 device
+$(call inherit-product, device/samsung/a12/device.mk)
+
+PRODUCT_DEVICE := a12
+PRODUCT_NAME := omni_a12
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A125F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a12nsxx-user 11 RP1A.200720.012 A125FXXU2BVG6 release-keys"
+
+BUILD_FINGERPRINT := samsung/a12nsxx/a12:11/RP1A.200720.012/A125FXXU2BVG6:user/release-keys
