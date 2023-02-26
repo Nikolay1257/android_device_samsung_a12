b44be92b7307d3ef99c7e70802aa557f45c66f44
Nikolay1257 <otraskinnikolaj@gmail.com>
2023-02-26 18:22:23 +0100

    add vibration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,8 +97,9 @@ PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
-# TWRP Configuration
+# TWRP Configuration : Basic config
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TW_USE_SAMSUNG_HAPTICS := true
